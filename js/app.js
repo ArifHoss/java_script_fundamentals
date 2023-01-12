b3f97946ceccf75a7db8186b63f5115e9d291009
Arif Hossain <arif.hossain@se.experis.com>
2023-01-12 13:37:11 +0100

    kata 1 result solved in different way, bit cleaner code

--- js/app.js
+++ js/app.js
@@ -1,45 +1,80 @@
-let word1 = document.getElementById("value1");
-let word2 = document.getElementById("value2");
-let word3 = document.getElementById("value3");
+// let word1 = document.getElementById("value1");
+// let word2 = document.getElementById("value2");
+// let word3 = document.getElementById("value3");
+//
+//
+//
+//
+//
+// function check (str){
+//
+//
+//   let result = "";
+//
+//   for(let char of str){
+//     if(char >= "A" && char <= "Z"){
+//       result += char;
+//     }
+//   }
+//
+//
+//   for(let char of str){
+//     if(char >= "a" && char <= "z"){
+//       result += char;
+//     }
+//   }
+//
+//   for(let char of str){
+//     if(char >= "0" && char <= "9"){
+//       result += char;
+//     }
+//   }
+//   return result;
+// }
+//
+//
+// check(word1)
+// check(word2)
+// check(word3)
+//
+// console.log(check(word1))
+//
+// console.log(check(word2))
+//
+// console.log(check(word3))
+//
+//
 
 
 
 
 
-function check (str){
-
+function reorder(string) {
 
   let result = "";
 
-  for(let char of str){
-    if(char >= "A" && char <= "Z"){
-      result += char;
-    }
-  }
+  let upperCase = "";
+  let lowerCase = "";
+  let number = "";
 
+  for (let char of string) {
 
-  for(let char of str){
-    if(char >= "a" && char <= "z"){
-      result += char;
-    }
-  }
+    if (char >= "A" && char <= "Z") {
+      upperCase += char;
 
-  for(let char of str){
-    if(char >= "0" && char <= "9"){
-      result += char;
+    }else if (char >= "a" && char <= "z") {
+      lowerCase += char;
+
+    }else if (char >= "0" && char <= "9") {
+      number += char;
     }
+
   }
-  return result;
+  return result = upperCase + lowerCase + number;
+
 }
 
-
-check(word1)
-check(word2)
-check(word3)
-
-console.log(check(word1))
-
-console.log(check(word2))
-
-console.log(check(word3))
+console.log(reorder("hA2p4Py"));
+console.log(reorder("m11oveMENT"));
+console.log(reorder("s9hOrt4CAKE"));
 
