339956014f365cc795cac32ca6a166540b266dfb
Arif Hossain <arif.hossain@se.experis.com>
2023-01-16 10:37:11 +0100

    show computer title, specs and description

--- index.js
+++ index.js
@@ -4,15 +4,16 @@ const payLoanElement = document.getElementById("pay");
 const bankElement = document.getElementById("bank");
 const workElement = document.getElementById("work");
 const laptopsElement = document.getElementById("laptops");
-const cartElement = document.getElementById("cart");
+const specsElement = document.getElementById("specs");
 const selectedLaptopElement = document.getElementById("selectedLaptop");
 const laptopDetailsElement = document.getElementById("laptopDetails");
 const laptopPriceElement = document.getElementById("laptopPrice");
 const buyNowElement = document.getElementById("buyNow");
+const imageElement = document.getElementById("image");
 
 
 let laptops = [];
-let cart = [];
+// let specs = [];
 let loanBalance = [];
 let bankBalance = [];
 let laptopPrice = [];
@@ -24,20 +25,30 @@ fetch("https://hickory-quilled-actress.glitch.me/computers")
 
 const addLaptopsToMenu = (laptops) => {
   laptops.forEach(x => addLaptopToMenu(x));
-  laptopPriceElement.innerText = laptops[0].price;
+  // laptopPriceElement.innerText = laptops[0].price;
 }
 
 
 const addLaptopToMenu= (laptop) =>{
   const laptopElement = document.createElement("option");
   laptopElement.value = laptop.id;
-  laptopElement.appendChild(document.createTextNode(laptop.description));
+  laptopElement.appendChild(document.createTextNode(laptop.title));
+
   laptopsElement.appendChild(laptopElement);
+
+
+  // laptopDetailsElement.appendChild(laptopElement);
 }
 
 const handleLaptopMenuChange = e =>{
   const selectedLaptop = laptops[e.target.selectedIndex];
   laptopPriceElement.innerText = selectedLaptop.price;
+  imageElement.src = `https://hickory-quilled-actress.glitch.me/${selectedLaptop.image}`
+  selectedLaptopElement.innerText = selectedLaptop.title
+  laptopDetailsElement.innerText = selectedLaptop.description;
+  specsElement.innerText = selectedLaptop.specs.join("\n ");
+
+
 }
 
 
