9ed96f4ff951d5faae20549655ce62d74bcb8854
Arif Hossain <arif.hossain@se.experis.com>
2023-01-16 14:06:40 +0100

    added work money to work bank and able to transfer to bank by clicking Bank button

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const loanBalanceElement = document.getElementById("loanBalance");
+const bankBalanceElement = document.getElementById("bankBalance");
 const loanElement = document.getElementById("loan");
-const payLoanElement = document.getElementById("pay");
+const paySalaryElement = document.getElementById("pay");
 const bankElement = document.getElementById("bank");
 const workElement = document.getElementById("work");
 const laptopsElement = document.getElementById("laptops");
@@ -43,8 +43,26 @@ const handleLaptopMenuChange = e =>{
   laptopDetailsElement.innerText = selectedLaptop.description;
   specsElement.innerText = selectedLaptop.specs.join("\n ");
 
+}
+
+const handleSalary = () => {
+  let paySalary = paySalaryElement.innerText;
+  let currentSalary = parseInt(paySalary);
+  paySalaryElement.innerText =  currentSalary + 100;
+}
+
+const handleBankBalance = () =>{
+  let loanBalance = parseInt(bankBalanceElement.innerText);
+  let paySalaryBalance = parseInt(paySalaryElement.innerText);
+
+
+  bankBalanceElement.innerText = loanBalance + paySalaryBalance;
+  paySalaryElement.innerText = 0;
+
+  console.log(bankBalanceElement);
 
 }
 
-
 laptopsElement.addEventListener("change", handleLaptopMenuChange);
+workElement.addEventListener("click", handleSalary);
+bankElement.addEventListener("click", handleBankBalance);
